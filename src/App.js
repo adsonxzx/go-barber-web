63abd31b03e426065bbe8ee3318d8bf83dac11e0
Adson Wesley <adsonxzx@gmail.com>
2020-01-11 11:03:34 -0300

    rotas privadas e estilizacao global

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes/index';
+import GlobalStyle from './styles/global';
 
 function App() {
-  return <h1>Go Barber</h1>;
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routes />
+    </BrowserRouter>
+  );
 }
 
 export default App;
